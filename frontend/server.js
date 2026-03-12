c619b2439a40ca743b9cbfde2fec87c9a4e26014
choudharivandana-ui <choudharivandana@gmail.com>
2026-03-12 16:56:32 +0530

    Fix wildcard route in server.js for Railway

--- frontend/server.js
+++ frontend/server.js
@@ -5,7 +5,8 @@ const app = express();
 // Serve Angular production build
 app.use(express.static(path.join(__dirname, 'dist/knowsphere')));
 
-app.get('*', (req, res) => {
+// Catch-all route for Angular
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/knowsphere/index.html'));
 });
 
